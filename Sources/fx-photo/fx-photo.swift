9dade7110d7ce78fee1403a1b7b6ee09804555ca
fan xian <fxean@icloud.com>
2025-09-24 21:36:53 +0800

    入口整理

--- Sources/fx-photo/fx-photo.swift
+++ Sources/fx-photo/fx-photo.swift
@@ -4,7 +4,6 @@ import ArgumentParser
 
 @main
 struct fx_photo: ParsableCommand {
-    // ArgumentParser提供的方法，直接写好配置就可以配置命令行
     static let configuration = CommandConfiguration(
         commandName: "fx-photo",
         abstract: "图片处理工具",
@@ -12,13 +11,14 @@ struct fx_photo: ParsableCommand {
                 支持多种图片处理功能，比如转换格式、追加图片等。
                 使用示例：
                   fx-photo convert <输入路径>
-                  fx-photo append <图片1> <图片2>
-                """,                                     // 可选，详细描述
-        version: "1.0.0",                         // 可选 --version
+                  fx-photo time "yyyy-mm-dd hh:mm:ss" <输入路径>
+                """,
+        version: "1.0.0",
         subcommands: [
-            Convert.self,                         // fx-photo convert ...
+            // fx-photo convert ...
+            Convert.self,
+            // fx-photo time "yyyy-mm-dd hh:mm:ss"
             Time.self,
-//            Append.self                           // fx-photo append ...
         ],
         defaultSubcommand: nil
     )
